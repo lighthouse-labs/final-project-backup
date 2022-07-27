fef526f4570a46fa030782d6f39b5d3c2a4aaab0
Robert812218 <robert812@shaw.ca>
2022-07-27 14:38:34 -0600

    update library page js/css

--- reverb-project/src/App.js
+++ reverb-project/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
  
 // function App() {
 //   return (
@@ -11,46 +11,18 @@ import './App.css';
 // }
  
 // export default App;
- 
-function LibraryPage() {
- return (
-   <div className="pageWrapper">
-     <header className="App-header">
-       <h1>Reverb</h1>
-     </header>
-     <div className="nav-bar-left">
-       <div className="navigation-button">Navigation</div>
-       <div className="search-button">Search</div>
-       <div className="library-button">Library</div>
-       <div className="songs-button">Songs</div>
-       <div className="albums-button">Albums</div>
-       <div className="playlists-button">Playlists</div>
-       <div className="podcasts-button">Podcasts</div>
-     </div>
- 
-     <div className="middle-section">
-       {/* <div className="user-display">Good morning, ${user}</div> */}
-       <div className="login-button">
-         <button>Login</button>
-       </div>
-       <div className="register-button">
-         <button>Register</button>
-       </div>
-       <div className="featured-playlists">Featured Playlists</div>
-       <div className="discover-playlist">Discover Weekly</div>
-       <div className="recent-playlist">Recently Played</div>
-     </div>
- 
-     <div className="nav-bar-right">
-       <div className="login-button">Login</div>
-       <div className="register-button">Register</div>
-       <div className="genres-button">Genres</div>
-       <div className="videos-button">Videos</div>
-       <div className="settings-button">Settings</div>
-     </div>
-     <div className="bottom-player"></div>
-   </div>
- );
-};
- 
-export default LibraryPage;
+
+///
+// import './App.css';
+import './styles/Library-page.css';
+import LibraryPage from './views/musiclibrary/Library-page';
+
+function App() {
+  return (
+    <div className="App">
+      <LibraryPage />
+    </div>
+  );
+}
+
+export default App;
